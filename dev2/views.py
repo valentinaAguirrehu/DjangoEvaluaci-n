752295dcccebcb5ca0cfab38505dcdd0019b07fe
Angie Benavides <angiebenavides1809@gmail.com>
2026-03-26 14:46:01 -0500

    Ultimas actualizaciones de mi hoja de vida Dev2

--- dev2/views.py
+++ dev2/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def hoja_vida_Angie(request):
-    return render(request, 'templates/dev2/index_Angie.html')
+def index_Angie(request):
+    return render(request, 'dev2/index_Angie.html')
